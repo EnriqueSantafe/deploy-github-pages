7ece9feacf8e1a2e0933ded3330741a0ec118752
Enrique <enrique.santafe@hotmail.com>
2022-05-24 14:26:33 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Web desplegada en Github Pages.
+          Web estática desplegada en Github Pages.
         </p>
         <a
           className="App-link"
